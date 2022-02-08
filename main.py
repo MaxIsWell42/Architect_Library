cadd95ec430e8d8d71d13378d551e385ed61d6be
MaxIsWell42 <maxfin13@gmail.com>
2022-02-08 15:44:48 -0500

    [add] gitignore and basic reading/search functionality

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
+from absl import app
+from absl import flags
+from os import path
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_bool('r', True, 'Read a job')
+flags.DEFINE_string('w', 'Name, Job Number, Notes', 'Add a new entry')
+flags.DEFINE_string('name', None, 'Job Name')
+flags.DEFINE_integer('number', None, 'Job Number')
+
+
+def main(argv):
+    if path.isfile('theLibrary.txt') != True:
+        open('theLibrary.txt')
+        
+    if FLAGS.r == True:
+        with open('theLibrary.txt', 'r') as file:
+            if FLAGS.name:
+                for line in file:
+                    if FLAGS.name.lower() in line.lower():
+                        print(line)
+                        
+            elif FLAGS.number:
+                for line in file:
+                    if FLAGS.number in line:
+                        print(line)
+                        
+    if FLAGS.w == True:
+        with open('theLibrary.txt', 'w') as file:
+            for line in file:
+                if line.isspace():
+                    file.write(FLAGS.w)
+                        
+                    
 
 if __name__ == "__main__":
-    f = open("theLibrary.txt", "w")
-    f.write("test")
-    f.close()
-    
-    l = open("theLibrary.txt", "r")
-    print(l.read())
+    app.run(main)
